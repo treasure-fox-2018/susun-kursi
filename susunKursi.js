9e7ec78c476c17ef8ccd62b627d8cf0fd92d8dd8
shantidyah <shanti.dyah@gmail.com>
2018-05-29 16:18:46 +0700

    susun kursi

--- susunKursi.js
+++ susunKursi.js
@@ -11,32 +11,38 @@
 'use strict'
 
 function generateSeats(row) {
-	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	var seats = [];
+	for(let i=0; i<row; i++) {
+		seats.push([]);
 	}
-	return seats
+	debugger;
+	// console.log(seats);
+	return seats;
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
-	let row = 0
-
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
-			row++
+	var seats = generateSeats(rowSeats);
+	debugger;
+	let row = 0;
+	for(let i=0; i<arr.length; i++) {
+		seats[row].push(arr[i]);
+		debugger;
+		// console.log(i);
+		if(row < 2) {
+			row++;
 		} else {
-			row == 0
+			row = 0;
 		}
+		// console.log(seats[row]);
 	}
-	printSeats(seats)
+	// console.log(seats);
+	printSeats(seats);
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
+	for(let i=0; i<seats.length; i++) {
 		console.log(`Baris ${i} : `, seats[i])
 	}
 }
 
-managePerson(['a','b','c','d','e','f','g','h','i','j'], 3)
+managePerson(['a','b','c','d','e','f','g','h','i','j'], 3);
